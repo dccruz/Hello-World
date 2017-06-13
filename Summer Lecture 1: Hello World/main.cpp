be8a8545d57d5689dbbb3be13424b32dbbd78b12
SMC <smc@DeVontes-MacBook-Pro.local>
2017-06-12 22:56:30 -0700

    Initial Commit

---
+++ Summer Lecture 1: Hello World/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Summer Lecture 1: Hello World
+//
+//  Created by SMC  on 6/12/17.
+//  Copyright © 2017 DC Cruz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
